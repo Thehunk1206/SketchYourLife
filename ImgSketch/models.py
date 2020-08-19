38ead9a0763677f9a1021ea2e705ec2ed5d006ea
Thomas Devyesh <thomas.devyesh.3a29@gmail.com>
2020-08-19 13:26:51 +0530

    initial commit frontend

---
+++ ImgSketch/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class SketchForLife(object):
+    def __init__(self,return_val):
+        self.return_val = return_val
+        print("aagayaaaaa ",self.return_val)
+    def retval(self):
+        return self.return_val
