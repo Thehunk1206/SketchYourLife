Minor fixes,fast computation,better performance

WebApp/ImgSketch/core/ImageToSketch.py
@@ -32,10 +32,11 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import numpy as np
 from numpy import ndarray
-from scipy.signal import convolve2d
 from PIL import Image
 
+from datetime import datetime
 
+#from WebApp.ImgSketch.core.ProcessImage import ImageProcess
 from ProcessImage import ImageProcess
 
 
@@ -43,17 +44,22 @@ class Image2Sketch(ImageProcess):
     """
     A Class to convert an image to sketch like image
 
-    args:
-        path - [str] Path to input image
+    Constructor args:
+        pathIn - [str] Path to input image
+        pathOut - [str] path to output image
+        nameOut - [str] name of the output image
         kernel_size - [int] size of the gaussian kernel for blurring
         sigma - [float] standard deviation for gaussian distribution
     """
 
-    def __init__(self, path: str, kernel_size: int = 21, sigma: float = 10.0):
+    def __init__(self, pathIn: str, pathOut: str, nameOut: str, kernel_size: int = 21, sigma: float = 10.0):
         super().__init__()
         self.__KERNEL_SIZE = kernel_size
         self.__SIGMA = sigma
-        self.path = path
+        self.__pathIn = pathIn
+        self.__pathOut = pathOut
+        self.__nameOut = nameOut
+        self.succes = False
 
     # getter setters
     def get_kernel_size(self) -> int:
@@ -68,33 +74,37 @@ class Image2Sketch(ImageProcess):
 
     # helper functions
 
-    def __adjust_height_width(self, img: ndarray) -> ndarray:
+    def _adjust_height_width(self, img: ndarray):
         """
         TODO Adjust the height and width(mainly downsample the image to speed up calculation)
         """
         pass
 
     # main function
-    def sketch_it(self) -> ndarray:
-
-        self.originalImg = super().loadImage(self.path)
-        self.originalImg = super()._rotate_image_90(self.originalImg, 1)
-
-        #self.adjstedImg = self.__adjust_height_width(self.originalImg)
+    def sketch_it(self) -> bool:
+        """
+        TODO Add full docstring here
+        """
+        self.originalImg = super().loadImage(self.__pathIn)
+        print("[info] Sketching your image....")
 
         self.grayImg = super().RGB2GRAY(self.originalImg)
-
         self.negativeImg = super().invertImage(self.grayImg)
         self.negativeBlur = super().gaussianBlur(
-                            img=self.negativeImg,
-                            kernel_size=self.__KERNEL_SIZE,
-                            sigma=self.__SIGMA)
+            img=self.negativeImg,
+            kernel_size=self.__KERNEL_SIZE,
+            sigma=self.__SIGMA)
 
+        # normalize gray image and negative blur in order to blend
+        self.grayImg = super()._normalize_img(self.grayImg)
+        self.negativeBlur = super()._normalize_img(self.negativeBlur)
         self.sketchImg = super().colorDodge(
-                            img1=self.negativeBlur,
-                            img2=self.grayImg)
-        return self.sketchImg
-    
+            img1=self.negativeBlur,
+            img2=self.grayImg)
+        self.succes = super().saveImage(self.sketchImg, self.__pathOut,
+                                        self.__nameOut+str(self.__KERNEL_SIZE))
+        return self.succes
+
     def __str__(self):
         return """
         A Class to convert an image to sketch like image
@@ -104,4 +114,13 @@ class Image2Sketch(ImageProcess):
             kernel_size - [int] size of the gaussian kernel for blurring
             sigma - [float] standard deviation for gaussian distribution
         """
-        
+
+
+# test code
+# TODO Remove this code from here
+im2sk = Image2Sketch(
+    "/home/tauhid/Desktop/Work space/SketchYouLife/sampleImage/robert2.jpg",
+    "/home/tauhid/Desktop/Work space/SketchYouLife/results", "result")
+im2sk.set_kernelsize_sigma(121, 50)
+im2sk.sketch_it()
+print(im2sk.succes)

WebApp/ImgSketch/core/ProcessImage.py
@@ -32,8 +32,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import numpy as np
 from numpy import ndarray
-from scipy.signal import convolve2d
+from cv2 import filter2D, imwrite
 from PIL import Image, UnidentifiedImageError
+
+import os
 
 
 class ImageProcess:
@@ -198,6 +200,10 @@ class ImageProcess:
             return None
         return self.img
 
+    def saveImage(self, img: ndarray, path: str, name: str, imageFormat: str = ".png") -> bool:
+        self.__isSaved = imwrite(os.path.join(path, name)+imageFormat, img)
+        return self.__isSaved
+
     def RGB2GRAY(self, img: ndarray) -> ndarray:
         """
         Converts a RGB image to Grayscale image
@@ -258,7 +264,7 @@ class ImageProcess:
 
     def gaussianBlur(self, img: ndarray, kernel_size: int = 21, sigma: float = 10.0) -> ndarray:
         """
-        NOTE: For now we use Scipy.sigmal.convolve2d and not naiveConvolve2d
+        NOTE: For now we use cv2.filter2d and not naiveConvolve2d to speed up computation
 
         Apply gaussian blurr on given image
 
@@ -270,9 +276,11 @@ class ImageProcess:
         return:
             blurred_img - [ndarray] blurred image 
         """
+        if not isinstance(img, ndarray):
+            raise "image should be in form of np.ndarray"
 
         self.__kernel = self._generate_gaussian_kernel(kernel_size, sigma)
-        self.__blurrImg = convolve2d(img, self.__kernel, mode='same')
+        self.__blurrImg = filter2D(img, -1, self.__kernel)
         self.__blurrImg = self._normalize_img(self.__blurrImg, range_end=255.0)
         return self.__blurrImg
 
@@ -282,14 +290,17 @@ class ImageProcess:
         It blends the image1 with image2 as background
 
         args:
-            img1 - [ndarray] image 1
-            img2 - [ndarray] image 2
+            img1 - [ndarray] image 1 should be normalized within the range (0,1)
+            img2 - [ndarray] image 2 should be normalized within the range (0,1)
 
         return:
             blended_img - [ndarray] Image1 blended with Image2x
         """
-        self.blended_img = img2/((256.0 - img1)+10e-12)
-        self.blended_img[self.blended_img > 255.0] = 255.0
+        if img1.max()>1.0 and img2.max()>1.0:
+            raise "np.ndarray should be normalized within the range (0,1)"
+        
+        self.blended_img = img2/((1.0 - img1)+10e-12)
+        self.blended_img[self.blended_img > 1.0] = 1.0
         self.blended_img = self._normalize_img(
             self.blended_img, range_end=255.0)
         return self.blended_img