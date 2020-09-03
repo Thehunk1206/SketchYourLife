7fcc5ba690703c96b0bf44acd0f316905febc375
ThomasDevyesh <thomas.devyesh.3a29@gmail.com>
2020-09-03 13:11:27 +0530

    objective complete

---
+++ WebApp/SketchYourLife/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('', views.home,name='home-page'),
+]
---
+++ WebApp/SketchYourLife/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+import os
+from pathlib import Path
+from PIL import Image
+
+def home(request):
+    try:    #TODOdelete from file system storage
+        if os.path.exists("./media/"):#"../WebApp/media/"
+            for f in os.listdir("./media/"):
+                os.remove(os.path.join("./media/", f))
+        else:
+            pass #TODOraise error and refresh page
+    finally:
+        if request.method == 'POST':
+            try:
+                ufile = request.FILES['ufile']
+            except:
+                return render(request, 'SketchYourLife/upload.html')
+            fs = FileSystemStorage()
+            fs.save(ufile.name, ufile)
+            openImg = Image.open(ufile)
+            finalImg = openImg.rotate(180)
+            finalImg.save("./SketchYourLife/static/SketchYourLife/sketched/"+ufile.name)#./WebApp
+
+            for a in os.listdir("./SketchYourLife/static/SketchYourLife/sketched/"):
+                sketched = os.path.join("static/SketchYourLife/sketched/",a)
+            
+            return render(request, 'SketchYourLife/sketch.html', {
+                'sketched': sketched
+            })
+        if os.path.exists("./SketchYourLife/static/SketchYourLife/sketched/"):#"../WebApp/media/"
+                for f in os.listdir("./SketchYourLife/static/SketchYourLife/sketched/"):
+                    os.remove(os.path.join("./SketchYourLife/static/SketchYourLife/sketched/", f))
+        else:
+            pass
+
+        return render(request, 'SketchYourLife/upload.html')
